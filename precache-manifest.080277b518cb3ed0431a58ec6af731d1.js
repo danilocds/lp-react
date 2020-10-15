c813dd13dafd12ed3b6a05f27d08c5886aacc7ed
danilocds <72626299+danilocds@users.noreply.github.com>
2020-10-15 04:28:49 -0300

    Add files via upload

---
+++ precache-manifest.080277b518cb3ed0431a58ec6af731d1.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad05d6e06598e11fa20feb0d63f31ab",
+    "url": "/promocao/on-demand-planos/index.html"
+  },
+  {
+    "revision": "44f310c11be77f070a80",
+    "url": "/promocao/on-demand-planos/static/css/main.6fbee537.chunk.css"
+  },
+  {
+    "revision": "741285aeb06747fbe791",
+    "url": "/promocao/on-demand-planos/static/js/2.d079d2be.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/promocao/on-demand-planos/static/js/2.d079d2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f310c11be77f070a80",
+    "url": "/promocao/on-demand-planos/static/js/main.317c106d.chunk.js"
+  },
+  {
+    "revision": "30b0e76bddfa9164b950",
+    "url": "/promocao/on-demand-planos/static/js/runtime-main.2a752bb1.js"
+  },
+  {
+    "revision": "80601a7ba24fae4b16452528e4add22a",
+    "url": "/promocao/on-demand-planos/static/media/ICONE_infinito_-03.80601a7b.png"
+  },
+  {
+    "revision": "9e176a1a86fb703b6e7ef9338c53b2cc",
+    "url": "/promocao/on-demand-planos/static/media/Mockup2@2x.9e176a1a.png"
+  },
+  {
+    "revision": "f84ea005889d498cdc9b0360eaa56d0a",
+    "url": "/promocao/on-demand-planos/static/media/brainIcon.f84ea005.svg"
+  },
+  {
+    "revision": "2b33f925aa4a386ffac9fc7cb2cb7e98",
+    "url": "/promocao/on-demand-planos/static/media/checkIcon.2b33f925.svg"
+  },
+  {
+    "revision": "d03aad528281caf7c6a0b9c1c96889e8",
+    "url": "/promocao/on-demand-planos/static/media/cienceIcon.d03aad52.svg"
+  },
+  {
+    "revision": "19e0af397f43fe5b0990bffbbc5a94a1",
+    "url": "/promocao/on-demand-planos/static/media/claudia-feitosa.19e0af39.png"
+  },
+  {
+    "revision": "d1d57e8e4668f974ba5d12897d8732ab",
+    "url": "/promocao/on-demand-planos/static/media/clovis-barros.d1d57e8e.jpg"
+  },
+  {
+    "revision": "eedda001722180839993074dbd1f7cca",
+    "url": "/promocao/on-demand-planos/static/media/contemporaryIcon.eedda001.svg"
+  },
+  {
+    "revision": "a8a2b23576c72c5534f4159cabbe39ff",
+    "url": "/promocao/on-demand-planos/static/media/gestaoIcon.a8a2b235.svg"
+  },
+  {
+    "revision": "26a44f326b5f7880bfc157fa4d11c540",
+    "url": "/promocao/on-demand-planos/static/media/greateQuestionsIcon.26a44f32.svg"
+  },
+  {
+    "revision": "b284882d5002d52364a2cbcb66af2fad",
+    "url": "/promocao/on-demand-planos/static/media/historyIcon.b284882d.svg"
+  },
+  {
+    "revision": "f9c11e469446a0f608beee2499eb274c",
+    "url": "/promocao/on-demand-planos/static/media/laptop2@2x.f9c11e46.png"
+  },
+  {
+    "revision": "04c319b07134d1cf2e02a946c4ce126a",
+    "url": "/promocao/on-demand-planos/static/media/literatureIcon.04c319b0.svg"
+  },
+  {
+    "revision": "8a95d619d8f15336473b7ee3dcfc3df2",
+    "url": "/promocao/on-demand-planos/static/media/luiz-hanss.8a95d619.jpg"
+  },
+  {
+    "revision": "7197e7a64ec04b0f340dad2d4f5f1ae2",
+    "url": "/promocao/on-demand-planos/static/media/maria-homem-min.7197e7a6.jpg"
+  },
+  {
+    "revision": "6f050464b61457e90a491d0c887a304d",
+    "url": "/promocao/on-demand-planos/static/media/mockup.6f043d9f@2x.6f050464.png"
+  },
+  {
+    "revision": "9b00c19c92bb8abdd23a40281c62bc13",
+    "url": "/promocao/on-demand-planos/static/media/movieIcon.9b00c19c.svg"
+  },
+  {
+    "revision": "5664f6b323b419a859168e3095effce3",
+    "url": "/promocao/on-demand-planos/static/media/musicIcon.5664f6b3.svg"
+  },
+  {
+    "revision": "183ff691ea9d4470aa3fd3695da525cf",
+    "url": "/promocao/on-demand-planos/static/media/paintingIcon.183ff691.svg"
+  },
+  {
+    "revision": "1e3521dad169b3a481200a21b7cea2d5",
+    "url": "/promocao/on-demand-planos/static/media/philosophyIcon.1e3521da.svg"
+  },
+  {
+    "revision": "50f933f6ed5c964941e3bd7217ac2bf8",
+    "url": "/promocao/on-demand-planos/static/media/religionIcon.50f933f6.svg"
+  },
+  {
+    "revision": "a708cb4b6bf58d8a20a4bcc3899ce247",
+    "url": "/promocao/on-demand-planos/static/media/selo-7dias.a708cb4b.png"
+  },
+  {
+    "revision": "99d452b242bf079bef3dd06f5d603339",
+    "url": "/promocao/on-demand-planos/static/media/startUpIcon.99d452b2.svg"
+  },
+  {
+    "revision": "9a9b083e5f7ce4ac960e38bea12d1a0e",
+    "url": "/promocao/on-demand-planos/static/media/theaterIcon.9a9b083e.svg"
+  },
+  {
+    "revision": "792d02b68fc577531d2e8d783ff86681",
+    "url": "/promocao/on-demand-planos/static/media/thinkersIcon.792d02b6.svg"
+  }
+]);
